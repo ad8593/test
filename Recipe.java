ef4bc211f48b657473461cf3ce401bb49d999126
ad8593 <ad8593@g.rit.edu>
2022-04-08 18:22:34 +0200

    recipe where we used the leaf pattern

---
+++ Recipe.java
@@ -0,0 +1,20 @@
+class Recipe implements ComponentRecipe{
+
+    private String name;
+    private String flname;
+    private int flcount;
+      
+    public Recipe(String name, String flname, int flcount)
+    {
+        this.name = name;
+        this.flname = flname;
+        this.flcount = flcount;
+    }
+      
+    public void showRecipeDetails() 
+    {
+        // take from the file and display 
+    }
+}
+
+// The Leaf
