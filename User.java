93e85f2d5b129d1a6223fdef6bfaf1fde9d9d269
ad8593 <ad8593@g.rit.edu>
2022-04-26 22:25:15 +0200

    phase2

---
+++ User.java
@@ -0,0 +1,11 @@
+public class User
+{
+    public static void main (String[] args)
+    {
+        AllTheFood keeper = new AllTheFood();
+          
+        GFoodMenu v = keeper.getGFoodMenu();
+        Both = keeper.getVegNonMenu();
+  
+    }
+}
