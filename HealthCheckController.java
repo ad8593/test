84b63f790083469cef18343e5a4520aff67999e0
ad8593 <ad8593@g.rit.edu>
2022-04-26 22:30:18 +0200

    phase2

--- HealthCheckController.java
+++ HealthCheckController.java
@@ -1,93 +1,40 @@
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.scene.control.Button;
+import javafx.stage.Stage;
 
-// health check controller is extending the view which is the gui 
-class HealthCheckController extends HealthCheckView{
-
-   // since the health check model extends the healthc check view, I did an object 
-   HealthCheckModel hm = new HealthCheckModel();
+class HealthCheckController {
+   private HealthCheckModel hcModel;
+   private HealthCheckView hcView;
    
-   public HealthCheckController() {
-      super();
+   public HealthCheckController(HealthCheckView _hcView, HealthCheckModel _hcModel){
+      hcView = _hcView;
+      hcModel = _hcModel;
+   
+      hcView.setButtonActionEvent(new ButtonActionEvent());
    }
-
-   // I put all event handler button actions in a separate methods
-   public void btnYes(){
-      this.btnYes.setOnAction
-            (
-         new EventHandler<ActionEvent>() {
-            public void handle(ActionEvent evt) {
-            // taking the method from the abstract class
-               hm.yesButton();
-            }});  
-   }    
-        
-   // methods same comment above for all 
-   public void btnNo(){ 
-      btnNo.setOnAction
-            (
-         new EventHandler<ActionEvent>() {
-            public void handle(ActionEvent evt) {
-               hm.noButton(); 
-            }
-         });            
+   
+   class ButtonActionEvent implements EventHandler<ActionEvent> {
+   
+      public void handle(ActionEvent evt) {
+      // Get the button that was clicked
+         Button btn = (Button)evt.getSource();
+                
+      // Switch on its name
+         switch(btn.getText()) {
+            case "Yes":
+               hcView.yesButtonClicked(hcModel.yesButtonClicked());
+               break;
+            case "Save Rec/Food":  
+               hcModel.sfClicked(hcView.sfClicked());  
+               System.out.println(hcView.sfClicked()); 
+               break;
+            case "Yes Delete":  
+              // hcView.buttonDeleteFoodClicked(hcModel.buttonDeleteFoodClicked());  
+               break;                
+            default:
+               System.exit(0);
+         }
+      }   
    }
-             
-   public void btnFod(){   
-      btnFod.setOnAction
-            (
-         new EventHandler<ActionEvent>() {
-            public void handle(ActionEvent evt) {
-               hm.buttonFood();
-            }
-            
-         });   
-   }      
-            
-   public void btnRec(){ 
-      btnRec.setOnAction
-            (
-         new EventHandler<ActionEvent>() {
-            public void handle(ActionEvent evt) {                         
-               hm.buttonRecipe();
-            }
-         });    
-   }
-            
-   public void btnSF(){   
-      btnSF.setOnAction
-            (
-         new EventHandler<ActionEvent>() {
-            public void handle(ActionEvent evt) {               
-               hm.foodItem(); 
-            }
-         });
-   }
-         
-   public void btnSD(){   
-      btnSD.setOnAction
-            (
-         new EventHandler<ActionEvent>() {
-            public void handle(ActionEvent evt) {               
-               hm.foodRecipe();
-            }
-         });
-   }
-         
-   public void btnYesDelete(){         
-      btnYesDelete.setOnAction
-            (
-         new EventHandler<ActionEvent>() {
-            public void handle(ActionEvent evt) {
-               hm.buttonDeleteFood();                       
-            }
-            
-         }); 
-   }
-         
-   // handle method 
-   public void handle(ActionEvent evt) {
-   }
-
-
 }
